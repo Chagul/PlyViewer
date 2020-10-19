c1f2d9c882112868749c43fe6d96e715b59b5489
Sulacox <56580857+Sulac0x@users.noreply.github.com>
2020-10-19 17:11:03 +0200

    modif taille ihm

--- sources-du-projet/src/modele/Main.java
+++ sources-du-projet/src/modele/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("../vue/view.fxml"));
         primaryStage.setTitle("3D Viewer");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 1280, 800));
         primaryStage.show();
     }
 
